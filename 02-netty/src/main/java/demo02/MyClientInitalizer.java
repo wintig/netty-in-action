be1d56bdbe89a1d68065d225f715a7c0a3b658e0
wintig <840872473@qq.com>
2019-09-24 21:54:17 +0800

    Nio Buffer学习

--- 02-netty/src/main/java/demo02/MyClientInitalizer.java
+++ 02-netty/src/main/java/demo02/MyClientInitalizer.java
@@ -3,8 +3,8 @@ package demo02;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
@@ -15,11 +15,14 @@ public class MyClientInitalizer extends ChannelInitializer<SocketChannel>{
     protected void initChannel(SocketChannel ch) throws Exception {
         ChannelPipeline pipeline = ch.pipeline();
 
-        // 解码器，分割接收到的byteBuffer
-        pipeline.addLast(new DelimiterBasedFrameDecoder(4096, Delimiters.lineDelimiter()));
+        // 解码器 二进制内容解析成真正携带的
+        pipeline.addLast(new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4, 0, 4));
+
+        // 编码器
+        pipeline.addLast(new LengthFieldPrepender(4));
+
         pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
         pipeline.addLast(new StringEncoder(CharsetUtil.UTF_8));
-
-        pipeline.addLast(null);
+        pipeline.addLast(new MyClientHandler());
     }
 }
